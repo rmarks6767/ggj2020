11c46f6257034fc320ab485523fb4eb45b8b2fce
John <j.yakubison@gmail.com>
2020-01-31 23:48:09 -0500

    Added Containment Constructor

--- ggj2020/Assets/Scripts/Containment.cs
+++ ggj2020/Assets/Scripts/Containment.cs
@@ -2,37 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Containment : Buildings
+
+namespace Assets.Scripts
 {
-    
-    private SCP[] safeQuantity;
-    private SCP[] euclidQuantity;
-    private SCP[] keterQuantity;
 
-    
-
-    public Containment()
+    public class Containment : Buildings
     {
-        // Floors, 
+
+        private int maxFloors;
+        private int maxCells;
+
+        /// <summary>
+        /// [Floor #, Cell #]
+        /// </summary>
+        private Cell[,] containmentFloors; 
         
-    }
 
-    /// <summary>
-    /// Adds an SCP to an aviliable 
-    /// </summary>
-    public void AddSCP()
-    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="maxFloors">Max floors per building</param>
+        /// <param name="maxCells">Max cells per floor</param>
+        public Containment(int maxFloors = 3, int maxCells = 6)
+        {
+            this.maxFloors = maxFloors;
+            this.maxCells = maxCells;
+            containmentFloors = new Cell[maxFloors, maxCells];
+        }
 
-    }
+        /// <summary>
+        /// Adds an SCP to an aviliable 
+        /// </summary>
+        public void AddSCP()
+        {
+
+        }
 
 
-    public override void Destroy()
-    {
-        throw new System.NotImplementedException();
-    }
+        public override void Destroy()
+        {
+            throw new System.NotImplementedException();
+        }
 
-    public override void Upgrade()
-    {
-        throw new System.NotImplementedException();
+        public override void Upgrade()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
