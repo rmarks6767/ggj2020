b5b15bef5edfb8f021926d8272c443bfc32ba274
Tori Rossini <tori.rossini42@gmail.com>
2020-02-01 00:42:54 -0500

    Title Scene and build settings edited

--- ggj2020/Assets/Scripts/UIManager.cs
+++ ggj2020/Assets/Scripts/UIManager.cs
@@ -9,18 +9,45 @@ namespace Assets.Scripts
         public GameObject PauseMenu;
         public GameObject GameplayUI;
         public GameObject BuildingPreviewDisplay;
+        public GameObject[] staffBuildings = new GameObject[3];
+        public GameObject[] containmentBuildings = new GameObject[3];
+        public GameObject[] researchBuildings = new GameObject[3];
+
+        private GameObject m_currentStaffBuilding;
+        private GameObject m_currentContainmentBuilding;
+        private GameObject m_currentResearchBuilding;
+
+
+
 
         // Start is called before the first frame update
         void Start()
         {
             PauseMenu.SetActive(false);
             GameplayUI.SetActive(true);
+            SetUpBuildingPreview();
         }
 
         // Update is called once per frame
         void Update()
         {
 
+        }
+
+        private void SetUpBuildingPreview()
+        {
+            m_currentStaffBuilding = staffBuildings[0];
+            m_currentContainmentBuilding = containmentBuildings[0];
+            m_currentResearchBuilding = researchBuildings[0];
+
+            m_currentStaffBuilding.SetActive(true);
+            m_currentContainmentBuilding.SetActive(true);
+            m_currentResearchBuilding.SetActive(true);
+        }
+
+        public bool UpdateBuilding()
+        {
+            return true;
         }
     }
 }
