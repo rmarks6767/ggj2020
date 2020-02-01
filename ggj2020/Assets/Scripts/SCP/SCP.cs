ce75d3361afcccc8dd6f8988386c373681b2f1fd
Corey <cbr7117@rit.edu>
2020-02-01 17:40:38 -0500

    Added Resources folder. Made SCPs render as game objects

--- ggj2020/Assets/Scripts/SCP/SCP.cs
+++ ggj2020/Assets/Scripts/SCP/SCP.cs
@@ -137,4 +137,6 @@ public class SCP
             return false;
         }
     }
+
+
 }
--- ggj2020/Assets/Scripts/SCP/SCPManager.cs
+++ ggj2020/Assets/Scripts/SCP/SCPManager.cs
@@ -9,7 +9,6 @@ public class SCPManager : MonoBehaviour
     int rCount; //researcher count in the entire facility
     int sCount; //security count in the entire facility
     public Containment building;
-    List<SCP> loadedScips;
 
     public int ResearcherCount
     {
@@ -39,7 +38,8 @@ public class SCPManager : MonoBehaviour
     void Start()
     {
         scips = new List<SCP>();
-        loadedScips = new List<SCP>();
+        loadAllSscips();
+        AddSCPToScene(5);
     }
 
     // Update is called once per frame
@@ -74,6 +74,7 @@ public class SCPManager : MonoBehaviour
                 {
                     cell.ContainSCP(scips[i]);
                     scips.RemoveAt(i);
+                    AddSCPToScene(i);
                     //remove the SCP from the list and add it to an empty cell
                 }
                 else
@@ -84,6 +85,14 @@ public class SCPManager : MonoBehaviour
                 return;
             }
         }
+    }
+
+    public void AddSCPToScene(int index)
+    {
+        GameObject newScip = new GameObject();
+        newScip.AddComponent<ScipObject>();
+        newScip.GetComponent<ScipObject>().number = scips[index].Number;
+        Instantiate(newScip, Vector3.zero, Quaternion.identity);
     }
 
     /// <summary>
@@ -113,37 +122,37 @@ public class SCPManager : MonoBehaviour
 
     void loadAllSscips()
     {
-        loadedScips.Add(new SCP(2, "Plague Doctor", "049", DangerLevel.euclid, false));
-        loadedScips.Add(new SCP(1, "[unknown]", "055", DangerLevel.safe, false));
-        loadedScips.Add(new SCP(2, "Cain", "073", DangerLevel.euclid, false));
-        loadedScips.Add(new SCP(2, "Able", "076", DangerLevel.euclid, false));
-        loadedScips.Add(new SCP(2, "Red Sea Object", "093", DangerLevel.euclid, false));
-        loadedScips.Add(new SCP(2, "The Shy Guy", "096", DangerLevel.euclid, false));
-        loadedScips.Add(new SCP(3, "The Old Man", "106", DangerLevel.keter, false));
-        loadedScips.Add(new SCP(1, "The Eye Pods", "131", DangerLevel.safe, false));
-        loadedScips.Add(new SCP(2, "Teenage Succubus", "166", DangerLevel.euclid, false));
-        loadedScips.Add(new SCP(2, "The Statue", "173", DangerLevel.euclid, false));
-        loadedScips.Add(new SCP(2, "The Coffee Machine", "294", DangerLevel.euclid, false));
-        loadedScips.Add(new SCP(1, "God", "343", DangerLevel.safe, false));
-        loadedScips.Add(new SCP(1, "Aggressive Skin Condition", "420", DangerLevel.safe, false));
-        loadedScips.Add(new SCP(1, "Panacea", "500", DangerLevel.safe, false));
-        loadedScips.Add(new SCP(1, "Josie the Half-Cat", "529", DangerLevel.safe, false));
-        loadedScips.Add(new SCP(1, "Butler's Hand Bell", "662", DangerLevel.safe, false));
-        loadedScips.Add(new SCP(4, "Hard-To-Destroy Reptile", "682", DangerLevel.keter, false));
-        loadedScips.Add(new SCP(2, "The Hanged King's Tragedy", "701", DangerLevel.euclid, false));
-        loadedScips.Add(new SCP(1, "The Jaded Ring", "714", DangerLevel.euclid, false));
-        loadedScips.Add(new SCP(3, "Maybe There Monsters", "870", DangerLevel.keter, false));
-        loadedScips.Add(new SCP(1, "Element-Switching Pills", "049", DangerLevel.safe, false));
-        loadedScips.Add(new SCP(1, "The Clockworks", "914", DangerLevel.safe, false));
-        loadedScips.Add(new SCP(3, "With Many Voices", "939", DangerLevel.keter, false));
-        loadedScips.Add(new SCP(1, "The Tickle Monster", "999", DangerLevel.safe, false));
-        loadedScips.Add(new SCP(3, "Bigfoot", "1000", DangerLevel.keter, false));
-        loadedScips.Add(new SCP(1, "Patches", "1424", DangerLevel.safe, false));
-        loadedScips.Add(new SCP(1, "The Gas Mask", "1499", DangerLevel.safe, false));
-        loadedScips.Add(new SCP(2, "Red Reality", "3001", DangerLevel.euclid, false));
-        loadedScips.Add(new SCP(2, "Garfield", "3166", DangerLevel.euclid, false));
-        loadedScips.Add(new SCP(2, "Revenge of the Redd Menace", "4601", DangerLevel.euclid, false));
-        loadedScips.Add(new SCP(3, "The Yule Man", "4666", DangerLevel.keter, false));
-        loadedScips.Add(new SCP(5, "Someone to Watch Over Us", "4999", DangerLevel.keter, false));
+        scips.Add(new SCP(2, "Plague Doctor", "049", DangerLevel.euclid, false));
+        scips.Add(new SCP(1, "[unknown]", "055", DangerLevel.safe, false));
+        scips.Add(new SCP(2, "Cain", "073", DangerLevel.euclid, false));
+        scips.Add(new SCP(2, "Able", "076", DangerLevel.euclid, false));
+        scips.Add(new SCP(2, "Red Sea Object", "093", DangerLevel.euclid, false));
+        scips.Add(new SCP(2, "The Shy Guy", "096", DangerLevel.euclid, false));
+        scips.Add(new SCP(3, "The Old Man", "106", DangerLevel.keter, false));
+        scips.Add(new SCP(1, "The Eye Pods", "131", DangerLevel.safe, false));
+        scips.Add(new SCP(2, "Teenage Succubus", "166", DangerLevel.euclid, false));
+        scips.Add(new SCP(2, "The Statue", "173", DangerLevel.euclid, false));
+        scips.Add(new SCP(2, "The Coffee Machine", "294", DangerLevel.euclid, false));
+        scips.Add(new SCP(1, "God", "343", DangerLevel.safe, false));
+        scips.Add(new SCP(1, "Aggressive Skin Condition", "420", DangerLevel.safe, false));
+        scips.Add(new SCP(1, "Panacea", "500", DangerLevel.safe, false));
+        scips.Add(new SCP(1, "Josie the Half-Cat", "529", DangerLevel.safe, false));
+        scips.Add(new SCP(1, "Butler's Hand Bell", "662", DangerLevel.safe, false));
+        scips.Add(new SCP(4, "Hard-To-Destroy Reptile", "682", DangerLevel.keter, false));
+        scips.Add(new SCP(2, "The Hanged King's Tragedy", "701", DangerLevel.euclid, false));
+        scips.Add(new SCP(1, "The Jaded Ring", "714", DangerLevel.euclid, false));
+        scips.Add(new SCP(3, "Maybe There Monsters", "870", DangerLevel.keter, false));
+        scips.Add(new SCP(1, "Element-Switching Pills", "049", DangerLevel.safe, false));
+        scips.Add(new SCP(1, "The Clockworks", "914", DangerLevel.safe, false));
+        scips.Add(new SCP(3, "With Many Voices", "939", DangerLevel.keter, false));
+        scips.Add(new SCP(1, "The Tickle Monster", "999", DangerLevel.safe, false));
+        scips.Add(new SCP(3, "Bigfoot", "1000", DangerLevel.keter, false));
+        scips.Add(new SCP(1, "Patches", "1424", DangerLevel.safe, false));
+        scips.Add(new SCP(1, "The Gas Mask", "1499", DangerLevel.safe, false));
+        scips.Add(new SCP(2, "Red Reality", "3001", DangerLevel.euclid, false));
+        scips.Add(new SCP(2, "Garfield", "3166", DangerLevel.euclid, false));
+        scips.Add(new SCP(2, "Revenge of the Redd Menace", "4601", DangerLevel.euclid, false));
+        scips.Add(new SCP(3, "The Yule Man", "4666", DangerLevel.keter, false));
+        scips.Add(new SCP(5, "Someone to Watch Over Us", "4999", DangerLevel.keter, false));
     }
 }
