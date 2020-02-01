3e14812c65591dec68e4485cdf855a41945effb3
Rhys Stever <rhys@stever.com>
2020-01-31 23:24:14 -0800

    Added some properties and override methods from the Buildings class

--- ggj2020/Assets/Scripts/Reseach.cs
+++ ggj2020/Assets/Scripts/Reseach.cs
@@ -4,22 +4,41 @@ using UnityEngine;
 using Assets.Scripts;
 
 
-public class Reseach : MonoBehaviour
+public class Reseach : Buildings
 {
-	public int level;
-	public List<ResearchStaff> researchers;
+	#region Fields
+	private int level;
+	private List<ResearchStaff> researchers;
+	#endregion
+
+	#region Properties
+	public int Level { get { return level; } }
+	public List<ResearchStaff> Researchers{	get { return researchers; }	}
+	#endregion
 
 	// Start is called before the first frame update
 	void Start()
     {
+		level = 1;
 		researchers = new List<ResearchStaff>();
     }
 
-    // Update is called once per frame
-    void Update()
+	// Update is called once per frame
+	void Update()
     {
         
     }
+
+	#region Methods
+	public override void Upgrade()
+	{
+		level++;
+	}
+
+	public override void Destroy()
+	{
+
+	}
 
 	public void Research(ResearchStaff researcher, SCP scp)
 	{
@@ -30,4 +49,5 @@ public class Reseach : MonoBehaviour
 	{
 
 	}
+	#endregion
 }
