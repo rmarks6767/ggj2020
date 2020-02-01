a8061c65686e69a1ab435aa12f1f1d4cff96c45c
John <j.yakubison@gmail.com>
2020-01-31 22:50:55 -0500

    Further work on contaiment, but created a cell class

---
+++ ggj2020/Assets/Scripts/Containment.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Containment : Buildings
+{
+    
+    private SCP[] safeQuantity;
+    private SCP[] euclidQuantity;
+    private SCP[] keterQuantity;
+
+    
+
+    public Containment()
+    {
+        // Floors, 
+        
+    }
+
+    /// <summary>
+    /// Adds an SCP to an aviliable 
+    /// </summary>
+    public void AddSCP()
+    {
+
+    }
+
+
+    public override void Destroy()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Upgrade()
+    {
+        throw new System.NotImplementedException();
+    }
+}
