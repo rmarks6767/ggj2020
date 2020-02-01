56bf92647e063b61a961090fdf51785d0d6abe55
Tori Rossini <tori.rossini42@gmail.com>
2020-01-31 23:32:43 -0500

    memes

--- ggj2020/Assets/Scripts/UIManager.cs
+++ ggj2020/Assets/Scripts/UIManager.cs
@@ -7,12 +7,14 @@ namespace Assets.Scripts
     public class UIManager : Singleton<UIManager>
     {
         public GameObject PauseMenu;
+        public GameObject GameplayUI;
         public GameObject BuildingPreviewDisplay;
 
         // Start is called before the first frame update
         void Start()
         {
-
+            PauseMenu.SetActive(false);
+            GameplayUI.SetActive(true);
         }
 
         // Update is called once per frame
