15e839c1a4192c1e9631826fe3e452be883fdda1
Rhys Stever <rhys@stever.com>
2020-02-02 01:36:35 -0800

    idk what i changed

--- ggj2020/Assets/Scripts/Money.cs
+++ ggj2020/Assets/Scripts/Money.cs
@@ -1,17 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Assets.Scripts;
 
 public class Money : MonoBehaviour
 {
 	#region Fields
-	// The GameManager Object
+	GameManager gm;
+	StaffManager sm;
 	#endregion
 
 	// Start is called before the first frame update
 	void Start()
     {
-        
+        // Initialize GameManager
+		// Initialize StaffManager
     }
 
     // Update is called once per frame
@@ -31,10 +34,10 @@ public class Money : MonoBehaviour
 	}
 	public void Payroll()
 	{
-		// Find # of researchers
+		// Find # of researchers from staff manager
 		// Multiply # of researchers by "salary" (cost per time)
 
-		// Find # of security
+		// Find # of security from staff manager
 		// Multiply # of security by "salary" (cost per time)
 
 		// Call LoseMoney() for total salary amount
--- ggj2020/Assets/Scripts/StaffManager.cs
+++ ggj2020/Assets/Scripts/StaffManager.cs
@@ -19,7 +19,7 @@ namespace Assets.Scripts
         // Start is called before the first frame update
         void Start()
         {
-            
+			
         }
 
         // Update is called once per frame
