a5617f94ee16c644b3a68424574104cce17c2e33
Rhys Stever <rhys@stever.com>
2020-02-02 02:29:18 -0800

    Finished Payroll method

--- ggj2020/Assets/Scripts/Money.cs
+++ ggj2020/Assets/Scripts/Money.cs
@@ -6,41 +6,91 @@ using Assets.Scripts;
 public class Money : MonoBehaviour
 {
 	#region Fields
+	// Managers
 	GameManager gm;
 	StaffManager sm;
+
+	// Timer/Payroll Variables
+	float startTime;
+	public float payrollRate;
 	#endregion
 
 	// Start is called before the first frame update
 	void Start()
     {
-        // Initialize GameManager
+		// Initialize GameManager
 		// Initialize StaffManager
+		startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Time.time - startTime <= payrollRate)
+		{
+			Payroll();
+		}
     }
 
 	#region Methods
 	public void GainMoney(int moneyGained)
 	{
 		// Adds moneyGained to money field of GameManager
+		gm.AddMoney(moneyGained);
 	}
 	public void LoseMoney(int moneyLost)
 	{
 		// Removes moneyLost to money field of GameManager
+		gm.AddMoney(-moneyLost);
 	}
 	public void Payroll()
 	{
-		// Find # of researchers from staff manager
-		// Multiply # of researchers by "salary" (cost per time)
+		int totalPayroll = 0;
 
-		// Find # of security from staff manager
-		// Multiply # of security by "salary" (cost per time)
+		// Loops thr both Dictionaries and 
+		foreach(KeyValuePair<int, GameObject> researchStaffPair in sm.researchStaff)
+		{
+			totalPayroll += FindPayRate(researchStaffPair.Value.GetComponent<Staff>());
+		}
+
+		foreach(KeyValuePair<int, GameObject> securityStaffPair in sm.securityStaff)
+		{
+			totalPayroll += FindPayRate(securityStaffPair.Value.GetComponent<Staff>());
+		}
 
 		// Call LoseMoney() for total salary amount
+		LoseMoney(totalPayroll);
+	}
+
+	/// <summary>
+	/// A helper method that finds the rate that the staff member needs to be paid
+	/// </summary>
+	/// <param name="staffMember">The staff member getting paid</param>
+	/// <returns></returns>
+	public int FindPayRate(Staff staffMember)
+	{
+		if(staffMember.type == StaffType.research)
+		{
+			return staffMember.tier * 15;
+		}
+		else if(staffMember.type == StaffType.security)
+		{
+			return staffMember.tier * 10;
+		}
+
+		return 0;
+	}
+
+	public void BuyStaff(StaffType staffType)
+	{
+		if(staffType == StaffType.research)
+		{
+			
+		}
+		else if(staffType == StaffType.security)
+		{
+
+		}
 	}
 	#endregion
 }
