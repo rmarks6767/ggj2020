2105e7bfe33b38fedccb53382b611a67ec5c3b60
Rhys Stever <rhys@stever.com>
2020-02-01 23:38:33 -0800

    Added sudo code with commits

---
+++ ggj2020/Assets/Scripts/Money.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money : MonoBehaviour
+{
+	#region Fields
+	// The GameManager Object
+	#endregion
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	#region Methods
+	public void GainMoney(int moneyGained)
+	{
+		// Adds moneyGained to money field of GameManager
+	}
+	public void LoseMoney(int moneyLost)
+	{
+		// Removes moneyLost to money field of GameManager
+	}
+	public void Payroll()
+	{
+		// Find # of researchers
+		// Multiply # of researchers by "salary" (cost per time)
+
+		// Find # of security
+		// Multiply # of security by "salary" (cost per time)
+
+		// Call LoseMoney() for total salary amount
+	}
+	#endregion
+}
